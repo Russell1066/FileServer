69291e63e9465f1860d7bcc559289710095d68df
Russell Woods <v-ruwoo@microsoft.com>
2018-01-22 09:20:15 -0800

    Add code that tests the conversion of an arbitrary type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,17 +15,40 @@ namespace ConsoleApp1
             public string Directory { get; set; }
             public string File { get; set; }
             public int Size { get; set; }
-            public int? Test { get; set; }
+            public int DownloadCount { get; set; }
             public bool DownloadPermitted { get; set; }
+
+            public BoolHolder Test { get; set; }
+
+            // BUGBUG remove
+            public class BoolHolder
+            {
+                public bool BoolValue { get; set; }
+
+                public static void RegisterBoolHolder()
+                {
+                    Resolver.AddConverter(s => new BoolHolder
+                    {
+                        BoolValue = bool.Parse(s)
+                    });
+                }
+            }
         };
 
         static void Main(string[] args)
         {
+            var s = new List<int> { 1, 2, 3, 4, };
+            var t = new List<int>(s) { 5, };
+
+            ImageInfo.BoolHolder.RegisterBoolHolder();
+            Resolver.TestPropertyExists = false;
+            Resolver.WrapExceptions = false;
             var v = Resolver.From<ImageInfo>();
+
             Console.WriteLine("Hello World!");
 
             ImageInfo imageInfo = new ImageInfo();
-            
+
         }
     }
 
